536d6b945aab0ffaaba6d16d80e736f390319b75
Jessica Meeker <113938036+jesmeeker@users.noreply.github.com>
2023-03-13 17:29:17 -0400

    login working

---
+++ src/components/art/ArtFeed.js
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { Post, Posts, TableRow } from "./Posts"
+import { getPosts, getSearchedPosts } from "../../managers/Posts"
+// import "./Posts.css"
+
+export const ArtFeed = ({ token, authorChoice, selectedCategory }) => {
+  const [posts, setPosts] = useState([])
+  const [filteredPosts, setFilteredPosts] = useState([])
+  const [searchTerms, setSearchTerms] = useState("Search Posts By Title")
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    getPosts().then((postData) => setPosts(postData))
+    setFilteredPosts(posts)
+  }, [])
+
+  useEffect(() => {
+    if (selectedCategory === 0 && authorChoice === 0) {
+      setFilteredPosts(posts)
+    } else if (selectedCategory !== 0 && authorChoice === 0) {
+      const filteredCopy = posts.filter(
+        (post) => post.category.id === parseInt(selectedCategory)
+      )
+      setFilteredPosts(filteredCopy)
+    } else if (selectedCategory === 0 && authorChoice !== 0) {
+      const filteredPostList = posts.filter(
+        (post) => post.author.id === parseInt(authorChoice)
+      )
+      setFilteredPosts(filteredPostList)
+    } else if (selectedCategory !== 0 && authorChoice !== 0) {
+      const filteredPostList = posts.filter(
+        (post) =>
+          post.author.id === parseInt(authorChoice) &&
+          post.category.id === parseInt(selectedCategory)
+      )
+      setFilteredPosts(filteredPostList)
+    }
+  }, [posts, selectedCategory, authorChoice])
+
+  const handleKeypress = (e) => {
+    //it triggers by pressing the enter key
+    if (e.keyCode === 13) {
+      handleSubmit()
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    getSearchedPosts(`${searchTerms}`).then((data) => setFilteredPosts(data))
+    setSearchTerms("Search Posts By Title")
+    document.getElementById("search").value = ""
+  }
+
+  return (
+    <>
+      <>
+        <section className="posts__buttons">
+          <section className="posts__filters">
+            <form>
+              <input
+                type="textfield"
+                placeholder={searchTerms}
+                id="search"
+                onChange={(e) => setSearchTerms(e.target.value)}
+                onKeyUp={handleKeypress}
+              ></input>
+              <button className="button is-small is-link is-rounded" type="submit" onClick={handleSubmit}>
+                Go
+              </button>
+              <button className="button is-small is-rounded" onClick={() => setFilteredPosts(posts)}>View All</button>
+            </form>
+          </section>
+        </section>
+      </>
+      <div>
+        {filteredPosts.map((post) => (
+          <Posts key={post.id} posts={post} token={token} />
+        ))}
+      </div>
+    </>
+  )
+}
