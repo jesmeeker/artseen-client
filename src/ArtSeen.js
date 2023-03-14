1fe992077a6f570b524090259e808e9299feb0a1
Jessica Meeker <113938036+jesmeeker@users.noreply.github.com>
2023-03-14 11:40:38 -0400

    register login logout working

--- src/ArtSeen.js
+++ src/ArtSeen.js
@@ -3,24 +3,17 @@ import { ApplicationViews } from "./views/ApplicationViews"
 import { NavBar } from "./components/nav/NavBar"
 
 
-//Function to set token state, Returns render NavBar and Application Views, passes token and setToken as props
 export const ArtSeen = () => {
-  // from mozilla: The getItem() method of the Storage interface, when passed a key name, 
-  // will return that key's value, or null if the key does not exist, in the given Storage object.
-  const [token, setTokenState] = useState(localStorage.getItem('auth_token'))
-  //what is the value of token when it's not an empty string? An object?
+    const [token, setTokenState] = useState(localStorage.getItem('artseen_token'))
 
-  // function setToken takes a single parameter of newToken
-  // calls localStorage.setItem method, which takes two arguments, for a key ('auth_token') and value (newToken) 
-  // Updates the values of setTokenState when invoked and passed an argument. Is invoked in components; Login (line 21 to login current user), 
-  // Registration(line 33 to login new user), and NavBar(line 49 to logout).
-  const setToken = (newToken) => {
-    localStorage.setItem('auth_token', newToken)
-    setTokenState(newToken)
-  }
+    const setToken = (newToken) => {
+        localStorage.setItem('artseen_token', newToken)
+        setTokenState(newToken)
+    }
 
-  return <>
-    <NavBar token={token} setToken={setToken} /> 
-    <ApplicationViews token={token} setToken={setToken} />
-  </>
+    return <>
+        <NavBar token={token} setToken={setToken} />
+        <ApplicationViews token={token} setToken={setToken} />
+    </>
 }
+
