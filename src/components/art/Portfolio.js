2e7b0649aea17a43e3c7666708891eb98a712944
Jessica Meeker <113938036+jesmeeker@users.noreply.github.com>
2023-03-15 09:34:58 -0400

    removed commented out code and cleaned up formatting

--- src/components/art/Portfolio.js
+++ src/components/art/Portfolio.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { getCurrentUserPieces } from "../../managers/Art";
 import { Art } from "./Art";
-// import { deletePosts, getCurrentUserPosts } from "../../managers/Posts";
 import "./art.css";
 
 export const Portfolio = ({ token }) => {
@@ -19,32 +18,13 @@ export const Portfolio = ({ token }) => {
         getMyPieces()
     }, [])
 
-    // const deleteWindow = (postId) => {
-    //     if (
-    //         window.confirm(
-    //             "Do you really want there to be one less potato post in the world?"
-    //         )
-    //     ) {
-    //         deletePosts(postId).then(() => {
-    //             {
-    //                 getMyPosts()
-    //             }
-    //         })
-    //     } else {
-    //         navigate(`/posts/myposts`)
-    //     }
-    // }
-
     return (
-        <><div className="art__container">
-                {/* <div className="tile is-ancestor"> */}
-                    
-                        {art.map((art_piece) => (
-                            <Art key={art_piece.id} art_piece={art_piece} token={token} />
-                        ))}
-                    
-                {/* </div> */}
-                </div>
+        <>
+            <div className="art__container">
+                {art.map((art_piece) => (
+                    <Art key={art_piece.id} art_piece={art_piece} token={token} />
+                ))}
+            </div>
         </>
     )
 }
