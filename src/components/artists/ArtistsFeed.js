7f73ef251ebe4225f0f0983c14503d133aab6bc2
Jessica Meeker <113938036+jesmeeker@users.noreply.github.com>
2023-03-21 19:29:23 -0500

    header added to artist view

--- src/components/artists/ArtistsFeed.js
+++ src/components/artists/ArtistsFeed.js
@@ -8,13 +8,28 @@ export const ArtistFeed = () => {
     useEffect(() => {
         getAllArtists().then((data) => setArtists(data))
     }, [])
-    return (
-        <>
+    return <>
+        <div className="container">
+            <nav className="level">
+
+            </nav>
+            <nav className="level bottom-border">
+                <div class="level-left">
+                    <div class="level-item">
+                        <p className="title is-1">artists</p>
+                    </div>
+                </div>
+                <div class="level-right" style={{}}>
+                    <div class="level-item">
+
+                    </div>
+                </div>
+            </nav>
             <div className="art__container">
                 {artists.map((artist) => (
-                    <Artist key={artist.id} artist={artist}  setArtists={setArtists}/>
+                    <Artist key={artist.id} artist={artist} setArtists={setArtists} />
                 ))}
             </div>
-        </>
-    )
+        </div>
+    </>
 }
